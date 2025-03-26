e29bf2b2aa132173af357624a077334e0d85fb84
Jake Ireland <jakewilliami@icloud.com>
2025-03-27 12:50:27 +1300

    Fix drift inaccuracy in counter!  v0.1.2
    Over time, inaccuracy in the counter drifts from actual time because
    of how the logic works.  Simple fix to change by calculating time
    elapsed based on single point in time at the start of the programme.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use std::sync::{
     atomic::{AtomicBool, Ordering},
     Arc, Mutex,
 };
-use std::{thread, time};
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
 
 #[derive(Parser)]
 #[command(name = "tt", author = crate_authors!("\n"), version = crate_version!())]
@@ -23,12 +26,6 @@ fn main() {
     let stdout = Arc::new(Mutex::new(stdout()));
     let s = stdout.clone();
 
-    // Overall seconds/partial seconds counters; used in combination
-    // with std::thread::sleep to measure time spent.  Note that we
-    // count up in 10ths of a second for precision.
-    let mut partial_seconds_counter: usize = 0;
-    let partials_in_second = 10;
-
     // Handle Ctrl + C:
     // https://rust-cli.github.io/book/in-depth/signals.html
     ctrlc::set_handler(move || {
@@ -45,42 +42,42 @@ fn main() {
     let mut stdout = stdout.lock().unwrap();
     stdout.execute(cursor::Hide).unwrap();
 
+    // Set start time measurement
+    let start_time = Instant::now();
+
     // Main counting/printing logic!
-    // Note that we count up in 100 milliseconds for better Ctrl+C precision/
-    // response time.  If we counted up in seconds, and the user pressed Ctrl+C
-    // as the second counter was incremented, they would have to wait close to
-    // one second before the programme stops.
+    //
+    // Note that we run the main event loop every 100 milliseconds for better
+    // Ctrl+C precision/response time.  If we counted up in seconds, and the
+    // user pressed Ctrl+C as the second counter was incremented, they would
+    // have to wait to close one second before the programme stops.
     while running.load(Ordering::SeqCst) {
-        // Check if our partial timer has added up to one second;
-        // if so, update stdout with the current expended time.
-        if (partial_seconds_counter % partials_in_second) == 0 {
-            // Calculate total seconds expended by the programme
-            let seconds = partial_seconds_counter / partials_in_second;
+        // Get elapsed time since start
+        let elapsed = start_time.elapsed();
+        let seconds = elapsed.as_secs();
 
-            // Write to time stdout:
-            // https://stackoverflow.com/a/59890400/12069968
-            stdout.queue(cursor::SavePosition).unwrap();
-            stdout
-                .write_all(format_seconds(seconds).as_bytes())
-                .unwrap();
-            stdout.queue(cursor::RestorePosition).unwrap();
-            stdout.flush().unwrap();
-            stdout.queue(cursor::RestorePosition).unwrap();
-            stdout
-                .queue(terminal::Clear(terminal::ClearType::FromCursorDown))
-                .unwrap();
-        }
+        // Write to time stdout:
+        // https://stackoverflow.com/a/59890400
+        stdout.queue(cursor::SavePosition).unwrap();
+        stdout
+            .write_all(format_seconds(seconds).as_bytes())
+            .unwrap();
+        stdout.queue(cursor::RestorePosition).unwrap();
+        stdout.flush().unwrap();
+        stdout.queue(cursor::RestorePosition).unwrap();
+        stdout
+            .queue(terminal::Clear(terminal::ClearType::FromCursorDown))
+            .unwrap();
 
-        // Sleep for one-tenth of a seecond
-        thread::sleep(time::Duration::from_millis(100));
-        partial_seconds_counter += 1;
+        // Sleep for one-tenth of a second
+        thread::sleep(Duration::from_millis(100));
     }
 
-    // Ensure cursor is shown before exitting
+    // Ensure cursor is shown before exiting
     stdout.execute(cursor::Show).unwrap();
 }
 
-fn format_seconds(seconds: usize) -> String {
+fn format_seconds(seconds: u64) -> String {
     let seconds_rem = seconds % 60;
     let minutes_rem = (seconds / 60) % 60;
     let hours_rem = (seconds / 60) / 60;
